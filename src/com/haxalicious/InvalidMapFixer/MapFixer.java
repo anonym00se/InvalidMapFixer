532e6a2922d5f218faf99176c32385c999b25b63
BuildTools <unconfigured@null.spigotmc.org>
2020-07-31 14:00:18 -0500

    Fixed container breaking bypass.

--- src/com/haxalicious/InvalidMapFixer/MapFixer.java
+++ src/com/haxalicious/InvalidMapFixer/MapFixer.java
@@ -2,15 +2,18 @@ package com.haxalicious.InvalidMapFixer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+@SuppressWarnings("deprecation")
 public class MapFixer implements Listener {
 	@EventHandler
-	public void onInventoryClickEvent(InventoryClickEvent event) {
+	public void onInventoryClickEvent(InventoryClickEvent event) { // Patch main exploit
 		try {
 			ItemStack item = event.getCurrentItem();
 			if(item.getType() == Material.MAP) { // Only check maps
@@ -22,6 +25,18 @@ public class MapFixer implements Listener {
 			}
 		} catch (NullPointerException npe) {
 			// Don't spam the log with errors
+		}
+	}
+	@EventHandler
+	public void onPlayerPickupItemEvent(PlayerPickupItemEvent event) { // Patch exploit through container breaking
+		Item droppedItem = event.getItem();
+		ItemStack item = droppedItem.getItemStack();
+		if(item.getType() == Material.MAP) { // Only check maps
+			if(Bukkit.getMap(item.getDurability()) == null) { // Check for invalid map
+				event.setCancelled(true);
+				int stackSize = item.getAmount();
+				droppedItem.setItemStack(new ItemStack(Material.MAP, stackSize, (short) 0)); // Revert map to ID 0
+			}
 		}
 	}
 }
